e84794fdc533c0f2fa7a7b7312f233220fd9d9bf
serii burduja <seriiburduja@gmail.com>
2025-05-16 11:56:55 +0300

    feat: admin and user seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = new Admin();
+        $admin->name = 'Admin';
+        $admin->email = 'admin@gmail.com';
+        $admin->password = bcrypt('12345678');
+        $admin->password_confirmation = bcrypt('12345678');
+    }
+}
