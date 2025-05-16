d379f123c5bba983855aba608456f7b137aff328
serii burduja <seriiburduja@gmail.com>
2025-05-16 12:29:36 +0300

    feat: admin guard

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,14 +10,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $guard = 'admin';
     protected $guarded = [
         'id',
         'remember_token',
@@ -27,21 +20,11 @@ class Admin extends Authenticatable implements MustVerifyEmail
         'updated_at',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
